aa19b0fb269a1a5c24ee32d20c744427f257994b
patekuru <druggiecosby@freenet.de>
2018-02-02 14:03:10 -0500

    better ui/ux. Terms.

--- view/home.php
+++ view/home.php
@@ -6,20 +6,26 @@
                     echo "<p style='font-weight: bold; color: red;'>" . $error['message']; "</p>";
                 }
                 ?>
-                <p><?php echo $lang['FORM_LOGIN']; ?></p>
-                <form action="index.php" method="POST" class="clearfix">
+<div class="col-md-6">
+                <p><strong><?php echo $lang['FORM_LOGIN']; ?></strong></p>
+                <form action="index.php" method="POST">
                     <input type="hidden" name="action" value="login" />
-                    <div class="col-md-2"><input type="text" class="form-control" name="username" placeholder="<?php echo $lang['FORM_USER']; ?>"></div>
-                    <div class="col-md-2"><input type="password" class="form-control" name="password" placeholder="<?php echo $lang['FORM_PASS']; ?>"></div>
-<div class="col-md-2"><input type "text" class="form-control" name="auth" placeholder="<?php echo $lang['FORM_2FA']; ?>"></div>
-                    <div class="col-md-2"><button type="submit" class="btn btn-default"><?php echo $lang['FORM_LOGIN']; ?></button></div>
+                    <input type="text" class="form-control" name="username" placeholder="<?php echo $lang['FORM_USER']; ?>"><br/>
+                    <input type="password" class="form-control" name="password" placeholder="<?php echo $lang['FORM_PASS']; ?>"><br/>
+<!-- <div class="col-md-2"><input type "text" class="form-control" name="auth" placeholder="<?php echo $lang['FORM_2FA']; ?>"></div>-->
+                    <button type="submit" class="btn btn-default"><?php echo $lang['FORM_LOGIN']; ?></button>
                 </form>
-                <br />
-                <p><?php echo $lang['FORM_CREATE']; ?></p>
-                <form action="index.php" method="POST" class="clearfix">
+</div>
+<div class="col-md-6">
+                <p><strong><?php echo $lang['FORM_CREATE']; ?></strong></p>
+                <form action="index.php" method="POST">
                     <input type="hidden" name="action" value="register" />
-                    <div class="col-md-2"><input type="text" class="form-control" name="username" placeholder="<?php echo $lang['FORM_USER']; ?>"></div>
-                    <div class="col-md-2"><input type="password" class="form-control" name="password" placeholder="<?php echo $lang['FORM_PASS']; ?>"></div>
-                    <div class="col-md-2"><input type="password" class="form-control" name="confirmPassword" placeholder="<?php echo $lang['FORM_PASSCONF']; ?>"></div>
-                    <div class="col-md-2"><button type="submit" class="btn btn-default"><?php echo $lang['FORM_SIGNUP']; ?></button></div>
+                    <input type="text" class="form-control" name="username" placeholder="<?php echo $lang['FORM_USER']; ?>"><br/>
+                    <input type="password" class="form-control" name="password" placeholder="<?php echo $lang['FORM_PASS']; ?>"><br/>
+                    <input type="password" class="form-control" name="confirmPassword" placeholder="<?php echo $lang['FORM_PASSCONF']; ?>"><br/>
+                    Terms: I understand that use of tremendouscoin.me is completely at my own risk, and that I will not be compensated in any way for any and all loss of my Tremendouscoins for any reason.
+                    Furthermore, I understand that Tremendouscoins may be removed from any account which has not been logged in to for 90 days or more. Finally, these terms may be modified without notice by being updated on this website.
+                    <p/>
+                    <button type="submit" class="btn btn-default">AGREE with Terms and SIGN UP</button>
                 </form>
+</div>
